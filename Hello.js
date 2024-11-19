e2557c2a795a706adb0f7be44cb034d1730f0c9d
vishwa-s-a <vishwasappaji769@gmail.com>
2024-11-19 15:11:58 +0530

    add linked list code

--- Hello.js
+++ Hello.js
@@ -1,3 +1,14 @@
 console.log('Hello git')
 
 console.log('Hello i am making these changes to commit -1')
+
+//i am implementing a linked list here
+function reverseList(){
+    console.log('I am reversing the linked list')
+
+}
+reverseList()
+
+function sayHello(){
+    console.log('I am saying hello to the world')
+}
